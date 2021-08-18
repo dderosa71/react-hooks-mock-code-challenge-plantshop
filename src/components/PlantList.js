3c484924b80db350826588778254efa60f08abd4
Dominic DeRosa <dderosa71@gmail.com>
2021-08-18 13:10:32 +0200

    Completed Core Deliverable

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React,{useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({search}) {
+
+  const [plants, setPlants] = useState([])
+
+  useEffect(()=>(fetch('http://localhost:6001/plants')
+  .then(resp => resp.json())
+  .then(json => setPlants(json))
+  .then(dom => console.log(dom))
+  ),[])
+
+  const filteredPlants = plants.filter((plant)=>plant.name.toLowerCase().includes(search.toLowerCase()))
+
+  const plantCardList = filteredPlants.map((plant)=> 
+  <PlantCard 
+  image={plant.image}
+  name={plant.name}
+  id={plant.id}
+  price={plant.price}
+  />
+  )
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantCardList}</ul>
   );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-function Search() {
+function Search({search, setSearch}) {
+
+
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
+        onChange={(event)=>setSearch(event.target.value)}
+        value={search}
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
       />
     </div>
   );
